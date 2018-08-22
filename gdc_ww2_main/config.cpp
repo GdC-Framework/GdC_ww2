8bab5058f70d582d75bfcd90454647866381b5e0
Sparfell <arsparfell19@gmail.com>
2018-08-22 20:18:33 +0200

    Adaptation pour IFA3 v30

--- gdc_ww2_main/config.cpp
+++ gdc_ww2_main/config.cpp
@@ -5,6 +5,14 @@ class CfgPatches
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = 0.10;
+		requiredAddons[] = {"ww2_core_c_if_zzz_lastloaded_c"};
+	};
+	// legacy
+	class gdc_ww2_radio
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.10;
 		requiredAddons[] = {};
 	};
 };
